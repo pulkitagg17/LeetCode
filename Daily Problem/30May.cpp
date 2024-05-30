71f74b2dfd5c61fcf41eef7eefb57531a5221080
pulkitagg17 <pulkitiscoding@gmail.com>
2024-05-30 23:19:16 +0530

    new approach

--- Daily Problem/30May.cpp
+++ Daily Problem/30May.cpp
@@ -53,3 +53,21 @@ public:
 };
 
 Approach 2: 
+class Solution {
+public:
+    int countTriplets(vector<int>& arr) {
+        int ans = 0;
+        int n = arr.size();
+
+        for(int i=0 ; i<n ; i++){
+            int val = arr[i];
+                for(int k=i+1 ; k<n ; k++ ){
+                    val ^= arr[k];
+
+                    if(val==0) 
+                        ans += (k-i);
+                }
+        }
+        return ans;
+    }
+};
